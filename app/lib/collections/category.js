11d0c93c07228b4f024ac725b833c936fd0ebb82
Jr Reyes <jr.evilboss@gmail.com>
2015-09-16 17:03:10 +0800

    Category collection added

---
+++ app/lib/collections/category.js
@@ -0,0 +1,41 @@
+var CategoriesSchema = new SimpleSchema({
+    name: {
+        type: String,
+        label: 'Name',
+        unique: true
+    }
+});
+
+Category = new Mongo.Collection('categories');
+Category.attachSchema(CategoriesSchema);
+
+
+if (Meteor.isServer) {
+    Category.allow({
+        insert: function (userId, doc) {
+            return true;
+        },
+
+        update: function (userId, doc, fieldNames, modifier) {
+            return true;
+        },
+
+        remove: function (userId, doc) {
+            return true;
+        }
+    });
+
+    Category.deny({
+        insert: function (userId, doc) {
+            return true;
+        },
+
+        update: function (userId, doc, fieldNames, modifier) {
+            return true;
+        },
+
+        remove: function (userId, doc) {
+            return true;
+        }
+    });
+}
