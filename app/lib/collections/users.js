03ae46ffe8ee5d7c33db6cf4347a58bc5294fc98
Jr Reyes <jr.evilboss@gmail.com>
2015-09-22 10:56:56 +0800

    cleanup code

--- app/lib/collections/users.js
+++ app/lib/collections/users.js
@@ -3,28 +3,19 @@
  */
 Meteor.users.after.insert(function (userId, doc) {
     var adminlist = Admins.find().fetch();
-    console.log(adminlist);
     if (doc.emails) {
         for (var email in doc.emails) {
-
-
             for (var admin in adminlist) {
-                console.log(doc);
-                console.log(adminlist[admin].email);
                 if (doc.emails[email].address === adminlist[admin].email) {
                     Meteor.users.update({_id: doc._id}, {$set: {roles: ['admin']}});
                     break;
                 }
-
             }
             var str = doc.emails[email].address;
             var name = str.match(/^([^@]*)@/)[1];
             if (name) {
                 Meteor.users.update({_id: doc._id}, {$set: {profile: {name: name}}});
             }
-
-
         }
     }
-
 });
