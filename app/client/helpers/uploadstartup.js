f3bb868a6e00bf63f27721e409b0e620103aa0d8
Jr Reyes <jr.evilboss@gmail.com>
2015-09-29 23:23:11 +0800

    added mozila plugin and inapp browser

--- app/client/helpers/uploadstartup.js
+++ app/client/helpers/uploadstartup.js
@@ -2,6 +2,8 @@
  * Created by aarondrup on 9/21/15.
  */
 Meteor.startup(function() {
+    Meteor.absoluteUrl.defaultOptions.rootUrl = "http://104.131.121.40";
+
     Uploader.uploadUrl = Meteor.absoluteUrl("upload"); // Cordova needs absolute URL
     Uploader.finished = function(index, fileInfo, templateContext) {
         Uploads.insert(fileInfo);
--- app/client/templates/library/book_info/book_layout/book_layout.js
+++ app/client/templates/library/book_info/book_layout/book_layout.js
@@ -16,105 +16,11 @@ Template.BookLayout.created = function () {
 
 Template.BookLayout.rendered = function () {
     var currentBook = Template.instance().data.file;
-    console.log(currentBook);
-    PDFJS.workerSrc = '/packages/pascoual_pdfjs/build/pdf.worker.js';
-// Create PDF
-    var pdfUrl =currentBook;
-    var url = pdfUrl;
-    var pdfDoc = null,
-        pageNum = 1,
-        pageRendering = false,
-        pageNumPending = null,
-        scale = 0.8,
-        canvas = document.getElementById('pdfcanvas'),
-        ctx = canvas.getContext('2d');
+    var ref = window.open('http://mozilla.github.io/pdf.js/web/viewer.html?file='+currentBook, '_blank','location=no');
+    ref.addEventListener('exit',function(event){
+        Router.go('library');
+    })
 
-    if (!window.requestAnimationFrame) {
-        window.requestAnimationFrame = (function () {
-            return window.webkitRequestAnimationFrame ||
-                window.mozRequestAnimationFrame ||
-                window.oRequestAnimationFrame ||
-                window.msRequestAnimationFrame ||
-                function (callback, element) {
-                    window.setTimeout(callback, 1000 / 60);
-                };
-        })();
-    }
-
-    PDFJS.disableStream = true;
-
-    function renderPage(num) {
-        pageRendering = true;
-        // Using promise to fetch the page
-        pdfDoc.getPage(num).then(function (page) {
-            var viewport = page.getViewport(scale);
-            canvas.height = viewport.height;
-            canvas.width = viewport.width;
-            // Render PDF page into canvas context
-            var renderContext = {
-                canvasContext: ctx,
-                viewport: viewport
-            };
-            var renderTask = page.render(renderContext);
-            // Wait for rendering to finish
-            renderTask.promise.then(function () {
-                pageRendering = false;
-                if (pageNumPending !== null) {
-                    // New page rendering is pending
-                    renderPage(pageNumPending);
-                    pageNumPending = null;
-                }
-            });
-        });
-        // Update page counters
-        document.getElementById('page_num').textContent = pageNum;
-    }
-
-    /**
-     * If another page rendering in progress, waits until the rendering is
-     * finised. Otherwise, executes rendering immediately.
-     */
-    function queueRenderPage(num) {
-        if (pageRendering) {
-            pageNumPending = num;
-        } else {
-            renderPage(num);
-        }
-    }
-
-    /**
-     * Displays previous page.
-     */
-    function onPrevPage() {
-        if (pageNum <= 1) {
-            return;
-        }
-        pageNum--;
-        queueRenderPage(pageNum);
-    }
-
-    document.getElementById('prev').addEventListener('click', onPrevPage);
-    /**
-     * Displays next page.
-     */
-    function onNextPage() {
-        if (pageNum >= pdfDoc.numPages) {
-            return;
-        }
-        pageNum++;
-        queueRenderPage(pageNum);
-    }
-
-    document.getElementById('next').addEventListener('click', onNextPage);
-    /**
-     * Asynchronously downloads PDF.
-     */
-    PDFJS.getDocument(url).then(function (pdfDoc_) {
-        pdfDoc = pdfDoc_;
-        document.getElementById('page_count').textContent = pdfDoc.numPages;
-        // Initial/first page rendering
-        renderPage(pageNum);
-    });
 };
 
 Template.BookLayout.destroyed = function () {
